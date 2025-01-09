fe1b33f68d96c4a46a7d90445e313e53c7657468
cori-o <ojd9512@gmail.com>
2025-01-09 18:16:40 +0900

    update file to wav code - m4a -> wav

--- file_to_wav.py
+++ file_to_wav.py
@@ -4,7 +4,6 @@ import os
 
 def main(args):
     audio_fp = AudioFileProcessor()
-    wav_file_name = args.file_name.split('.')[0] + '.wav'
     file_path = os.path.join(args.data_path, args.file_name)
     audio_fp.m4a_to_wav(file_path)
 
@@ -12,8 +11,6 @@ def main(args):
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
     cli_parser.add_argument('--data_path', type=str, default='./data')
-    cli_parser.add_argument('--output_path', type=str, default='./data/output')
     cli_parser.add_argument('--file_name', type=str, default=None)
-    cli_parser.add_argument('--file_type', type=str, default='pcm')
     cli_args = cli_parser.parse_args()
     main(cli_args)
