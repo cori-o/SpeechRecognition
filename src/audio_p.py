8f3224a86979c485f8fa50f87258ed3bcc8b8514
cori-o <ojd9512@gmail.com>
2025-01-09 10:22:46 +0900

    update diar module - calc duration

--- src/audio_p.py
+++ src/audio_p.py
@@ -22,160 +22,29 @@ import io
 import os
 
 
-class DataProcessor:
-    def data_to_df(self, dataset, columns):
-        if isinstance(dataset, list):
-            return pd.DataFrame(dataset, columns=columns)
-       
-    def df_to_hfdata(self, df):
-        return Dataset.from_pandas(df)
-
-    def merge_data(self, df1, df2, how='inner', on=None):
-        return pd.merge(df1, df2, how='inner')
-
-    def filter_data(self, df, col, val):
-        return df[df[col]==val].reset_index(drop=True)
-    
-    def remove_keywords(self, df, col, keyword=None, exceptions=None):
-        if exceptions != None: 
-            if keyword != None:
-                # pattern = r'(?<![\w가-힣])(?:' + '|'.join(map(re.escape, keyword)) + r')(?![\w가-힣])'
-                pattern = re.compile(r'(?<![\w가-힣])(' + '|'.join(map(re.escape, val)) + r')(?=[^가-힣]|$)')
-            else:
-                pattern = r'(?<![\w가-힣])(\S*주)(?![\w가-힣])'    # 테마주 같은 함정 증권 종목 제거 
-            mask = df[col].str.contains(pattern, na=False) & ~df[col].str.contains('|'.join(map(re.escape, exceptions)), na=False)
-            df = df[~mask]
-            return df.reset_index(drop=True)
-        else: 
-            keyword_idx = df[df[col].str.contains(keyword, na=False)].index 
-            df.drop(keyword_idx, inplace=True)
-            return df.reset_index(drop=True)
-        
-    def save_results_to_pickle(self, result, output_file):
-        with open(output_file, "wb") as f:
-            pickle.dump(result, f)
-        print(f"Results saved to {output_file}")
-
-    def load_results_from_pickle(self, input_file):
-        with open(input_file, "rb") as f:
-            result = pickle.load(f)
-        print(f"Results loaded from {input_file}")
-        return result
-
-
-class TextProcessor:
-    def count_pattern(self, text, patterns):
-        cnt = 0 
-        for pattern in sorted(patterns, reverse=True):
-            if pattern in text: 
-                cnt += 1 
-                text = text.replace(pattern, '', 1)
-        return cnt 
-                   
-    def remove_duplications(self, text):
-        '''
-        줄바꿈 문자를 비롯한 특수 문자 중복을 제거합니다.
-        '''
-        text = re.sub(r'(\n\s*)+\n+', '\n\n', text)    # 다중 줄바꿈 문자 제거
-        text = re.sub(r"\·{1,}", " ", text)    
-        return re.sub(r"\.{1,}", ".", text)
-        
-    def remove_patterns(self, text, pattern):
-        '''
-        입력된 패턴을 제거합니다. 
-        pattern:
-        r'^\d+\.\s*': "숫자 + ." 
-        r"(뉴스|주식|정보|분석)$": 삼성전자뉴스 -> 삼성전자
-        '''
-        return re.sub(pattern, '', text)
-    
-    def check_expr(self, expr, text):
-        '''
-        expr 값이 text에 있는지 검사합니다. 있다면 True를, 없다면 False를 반환합니다. 
-        '''
-        return bool(re.search(expr, text))
-    
-    def get_val(self, val, text):
-        '''
-        expr 값이 text에 있으면 반환합니다.  
-        '''
-        if isinstance(val, str):
-            return re.search(rf'\b{re.escape(val)}\b')
-        elif isinstance(val, list):
-            return [re.search(rf'\b{re.escape(v)}\b') for v in val]
-    
-    def get_val_with_indices(self, val, text):
-        '''
-        val 값이 text에 있으면 시작과 끝 위치 정보와 함께 값을 반환합니다.
-        '''
-        found_stocks = []
-        if isinstance(val, str):
-            pattern = rf'(^|[^a-zA-Z0-9가-힣]){re.escape(val)}($|[^a-zA-Z0-9가-힣])'
-            matches = list(re.finditer(pattern, text))
-            for match in matches:
-                # 매칭된 텍스트에서 실제 단어의 시작과 끝 위치 조정
-                start = match.start() + (1 if match.group(1) else 0)
-                end = match.end() - (1 if match.group(2) else 0)
-                found_stocks.append((val, start, end))
-            return found_stocks
-        elif isinstance(val, list):
-            pattern = re.compile(r'\b(?:' + '|'.join(map(re.escape, val)) + r')\b')
-            matches = [(match.group(), match.start(), match.end()) for match in pattern.finditer(text)]
-            return matches 
-            
-    def check_l2_threshold(self, txt, threshold, value):
-        '''   
-        threshold 보다 값이 높은 경우, 모르는 정보로 간주합니다. 
-        '''
-        print(f'Euclidean Distance: {value}, Threshold: {threshold}')
-        return "모르는 정보입니다." if value > threshold else txt
-    
-
 class TimeProcessor:
-    def get_previous_day_date(self):
-        '''
-        전일 연도, 월, 일을 반환합니다.   
-        returns: 
-        20240103
-        '''
-        now = datetime.now()
-        yesterday = now - timedelta(days=1)
-        return str(yesterday.year), str(yesterday.month).zfill(2), str(yesterday.day).zfill(2)
-
-    def get_current_date(self):
-        '''
-        현재 연도, 월, 일을 반환합니다.
-        '''
-        now = datetime.now()
-        return str(now.year), str(now.month).zfill(2), str(now.day).zfill(2)
-
-    def get_current_time(self):
-        return datetime.now()
-
-    def is_similar(diar_seg, stt_seg):
+    def is_similar(diar_seg, stt_result):
         '''
         두 세그먼트 간 겹치는 길이와 발화 시간이 유사한지 검사
         '''
+        TIME_TOLERANCE = 1.5   # 허용 오차(초)
         diar_start, diar_end = diar_seg['start'], diar_seg['end']
-        stt_start, stt_end = stt_seg['start_time'], stt_seg['end_time']
+        stt_start, stt_end = stt_result['start_time'], stt_result['end_time']
 
         diar_duration = diar_end - diar_start
         stt_duration = stt_end - stt_start
-
-        TIME_TOLERANCE = 1.5   # 허용 오차(초)
-
+    
         # 겹치는 구간 계산
         overlap_start = max(diar_start, stt_start)
         overlap_end = min(diar_end, stt_end)
         overlap_duration = max(0, overlap_end - overlap_start)
-        # print(diar_seg, stt_seg)
         print(f'overlap duration: {overlap_duration}, abs: {abs(diar_duration - stt_duration)}')
 
-        # 조건: 겹침이 충분히 길고, 발화 시간도 비슷해야 함
-        if overlap_duration > 0.5 and abs(diar_duration - stt_duration) < TIME_TOLERANCE:
+        if overlap_duration > 1 and abs(diar_duration - stt_duration) < TIME_TOLERANCE:
             return True
         else:
             return False
+
 
 class AudioFileProcessor:
     def align_audio(self, reference_file, target_file, output_file):
@@ -277,13 +146,6 @@ class NoiseHandler:
         wav_buffer.seek(0)   # 파일 포인터를 처음으로 이동
         return wav_buffer
 
-    def reduce_noise(self, audio_file, sr, y_noise):
-        '''
-        
-        '''
-        nr.reduce_noise(y=audio_samples, sr=audio.frame_rate, y_noise=noise_sample)
-        
-
     def filter_audio_with_pydub(self, input_file, high_cutoff=200, low_cutoff=3000, output_file=None):
         '''
         pydub (python)을 이용한 오디오 필터링 (고역대, 저역대)
@@ -382,7 +244,6 @@ class VoiceEnhancer:
         amplified_audio.export(output_file, format="wav")
         print(f"Amplified audio saved to {output_file}")
         return amplified_audio
-
 
     def emphasize_nearby_voice(self, input_file, threshold=0.05, output_file=None):
         """
@@ -518,7 +379,7 @@ class SpeakerDiarizer:
                 speak_duration = seg['end'] - seg['start']
                 speak_time += speak_duration 
                 cnt += 1
-        print(f'{speaker}: {round((speak_time / cnt), 2)}초')
+        return speak_time, round((speak_time / cnt), 2)
 
     def convert_segments(self, result):
         """
