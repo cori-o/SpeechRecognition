9ec71247cfa64cf7858d771cb5ea6666a8050c5c
cori-o <ojd9512@gmail.com>
2025-01-19 23:49:51 +0900

    update audio_p (algorithm update of result mapping func)

--- src/audio_p.py
+++ src/audio_p.py
@@ -489,6 +489,7 @@ class SpeakerDiarizer:
         diar_result = self.rename_speaker(results)
         return diar_result
 
+
 class ResultMapper:
     def map_speaker_with_nested_check(self, time_p, stt_result, diar_segments, meeting_id=None, table_editor=None):
         """ stt_result: conv_id, start_time, end_time, text, conf_id """
@@ -506,6 +507,8 @@ class ResultMapper:
             print("No overlapping segments found.")
             return    # DB 업데이트 x 
 
+        max_overlap = 0
+        best_speaker = None 
         for candidate in candidates:
             diar_start, diar_end = candidate['start'], candidate['end']
             nested_segments = [     #  Diar 구간 내에 또 다른 구간 탐색 (0~19 -> 13~14)
@@ -521,18 +524,17 @@ class ResultMapper:
                         else:
                             updated_result = (conv_id, nested['speaker'])
                             table_editor.edit_poc_conf_log_tb('update', 'ibk_poc_conf_log', data=meeting_id, val=updated_result)
-                            return 
+                            return
             # 또 다른 발화가 없을 때 (겹치는 후보군들만 탐색)
-            max_overlap = 0
-            for diar_seg in candidates:
-                overlap_start = max(stt_start, diar_seg['start'])
-                overlap_end = min(stt_end, diar_seg['end'])
-                overlap_duration = max(0, overlap_end - overlap_start)
-                if overlap_duration > max_overlap:
-                    max_overlap = overlap_duration
-                    updated_result = (conv_id, diar_seg['speaker'])
-                    table_editor.edit_poc_conf_log_tb('update', 'ibk_poc_conf_log', data=meeting_id, val=updated_result)
-                    return 
+            overlap_start = max(stt_start, diar_start)
+            overlap_end = min(stt_end, diar_end)
+            overlap_duration = max(0, overlap_end - overlap_start)
+            if overlap_duration > max_overlap:
+                max_overlap = overlap_duration 
+                best_speaker = candidate['speaker']
+        if best_speaker:
+            updated_result = (conv_id, best_speaker)
+            table_editor.edit_poc_conf_log_tb('update', 'ibk_poc_conf_log', data=meeting_id, val=updated_result)
             return 
 
 class ETC:
