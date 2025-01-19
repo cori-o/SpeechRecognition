ef5e6ccdc82b3197f1f01df911083757ed4d895f
cori-o <ojd9512@gmail.com>
2025-01-20 00:10:44 +0900

    add rename test.py

---
+++ rename_test.py
@@ -0,0 +1,23 @@
+from src import SpeakerDiarizer
+from dotenv import load_dotenv
+import argparse
+import json
+import os
+
+def main(args):
+    load_dotenv()
+    hf_api_key = os.getenv('HF_API')
+
+    speaker_diarizer = SpeakerDiarizer()
+    speaker_diarizer.set_pyannotate(hf_api_key)
+    with open(os.path.join('./data', args.file_name), "r", encoding="utf-8") as f:
+        diar_result = json.load(f)
+    print(speaker_diarizer.rename_speaker(diar_result, 4))
+
+if __name__ == '__main__':
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument('--output_path', type=str, default='./data/output')
+    cli_parser.add_argument('--chunk_length', type=int, default=None)
+    cli_parser.add_argument('--file_name', type=str, default=None) 
+    cli_args = cli_parser.parse_args()
+    main(cli_args)
